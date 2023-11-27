55e824da30f1cec245e8bf59a36d4ebc3c87c123
Mayra Cortez <45056239+mc0x@users.noreply.github.com>
2023-11-27 14:34:51 -0800

    Add files via upload

---
+++ Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Mayra Cortez on 7/16/23.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+    static let repos = "folder"
+    static let gists = "text.alignleft"
+    static let followers = "heart"
+    static let following = "person.2"
+}
